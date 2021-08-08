7f3da2cc048b26892c836a29bce786f9efc52cfc
TimothySiskin <tymoteuszczyz@gmail.com>
2021-08-08 18:18:13 +0200

    +chenged the order of links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,21 +42,18 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+
+            <li className="nav-item">
               <Link
                 to="/services"
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/products"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Products
               </Link>
             </li>
 
