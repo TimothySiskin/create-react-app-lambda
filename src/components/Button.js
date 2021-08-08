aa05b8586129cc76b5a2cde9423f48ce54b76901
TimothySiskin <tymoteuszczyz@gmail.com>
2021-08-08 14:16:45 +0200

    +typo fix

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ export const Button = ({
     ? buttonStyle
     : STYLES[0];
 
-  const checkButtonSize = SIZE.includes(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
     <Link to="sing-up" className="btn-mobile">
