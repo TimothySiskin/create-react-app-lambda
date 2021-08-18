9ac3de2b82f0bdbe121ac942b9c90e9babe91582
TimothySiskin <tymoteuszczyz@gmail.com>
2021-08-18 07:29:54 +0200

    - video tag

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,6 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>Best in CEO BRANDING </h1>
       <p>Built on Referrals and Inspired By Trust Since 2018</p>
       <div className="hero-btns">
