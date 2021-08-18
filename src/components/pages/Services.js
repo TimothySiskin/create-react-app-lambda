5c5be08686cbb67a941f65f8345f39fe2acd778f
TimothySiskin <tymoteuszczyz@gmail.com>
2021-08-18 07:29:38 +0200

    + architecture

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Footer from "../Footer";
 
 export default function Services() {
-  return <h1 className="about">Services</h1>;
+  return (
+    <>
+      <h1 className="about">Services</h1>
+      {/* {Grid base layout 3x3 widt color gradation tiles} */}
+      <Footer />
+    </>
+  );
 }
